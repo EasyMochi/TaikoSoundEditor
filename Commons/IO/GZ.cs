010802a66fb0a10bcc79994c4932152f3ca17ee6
EasyMochi <7991344+EasyMochi@users.noreply.github.com>
2026-07-22 19:26:51 +0100

    Use built-in gzip compression

--- Commons/IO/GZ.cs
+++ Commons/IO/GZ.cs
@@ -1,4 +1,3 @@
-﻿using ICSharpCode.SharpZipLib.GZip;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
@@ -6,53 +5,44 @@ using TaikoSoundEditor.Commons.Utils;
 
 namespace TaikoSoundEditor.Commons.IO
 {
-    internal class GZ
+    internal static class GZ
     {
-
         public static string DecompressBytes(byte[] bytes)
         {
             Logger.Info("GZ Decompressing bytes");
-            using (MemoryStream ms = new MemoryStream(bytes))
-            using (GZipStream decompressionStream = new GZipStream(ms, CompressionMode.Decompress))
-            using (StreamReader reader = new StreamReader(decompressionStream))
-                return reader.ReadToEnd();
+            using var input = new MemoryStream(bytes);
+            using var gzip = new GZipStream(input, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzip);
+            return reader.ReadToEnd();
         }
 
         public static string DecompressString(string gzPath)
         {
             Logger.Info("GZ Decompressing string");
-
-            using (FileStream originalFileStream = File.OpenRead(gzPath))
-            using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
-            using (StreamReader reader = new StreamReader(decompressionStream))
-                return reader.ReadToEnd();
+            using var input = File.OpenRead(gzPath);
+            using var gzip = new GZipStream(input, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzip);
+            return reader.ReadToEnd();
         }
 
         public static byte[] DecompressBytes(string gzPath)
         {
             Logger.Info("GZ Decompressing bytes");
-            using (FileStream originalFileStream = File.OpenRead(gzPath))
-            using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
-            using (MemoryStream ms = new MemoryStream())
-            {
-                decompressionStream.CopyTo(ms);
-                return ms.ToArray();
-            }
+            using var input = File.OpenRead(gzPath);
+            using var gzip = new GZipStream(input, CompressionMode.Decompress);
+            using var output = new MemoryStream();
+            gzip.CopyTo(output);
+            return output.ToArray();
         }
 
         public static byte[] CompressToBytes(string content)
         {
             Logger.Info("GZ Compressing bytes");
             var uncompressed = Encoding.UTF8.GetBytes(content);
-            using (MemoryStream outStream = new MemoryStream())
-            {
-                using (GZipOutputStream gzoStream = new GZipOutputStream(outStream))
-                {
-                    gzoStream.SetLevel(5);
-                    gzoStream.Write(uncompressed, 0, uncompressed.Length);
-                }
-                return outStream.ToArray();
-            }
-        }       
+            using var output = new MemoryStream();
+            using (var gzip = new GZipStream(output, CompressionLevel.Optimal, leaveOpen: true))
+                gzip.Write(uncompressed, 0, uncompressed.Length);
+            return output.ToArray();
+        }
     }
 }
