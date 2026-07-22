8349c6b49ce1f20a6e1a51c5a13c3747a9f34386
EasyMochi <7991344+EasyMochi@users.noreply.github.com>
2026-07-22 18:44:55 +0100

    Expose project diagnostics in the editor

---
+++ MainForm.Diagnostics.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Windows.Forms;
+using TaikoSoundEditor.Commons.Utils;
+using TaikoSoundEditor.Project;
+
+namespace TaikoSoundEditor
+{
+    partial class MainForm
+    {
+        private ToolStripMenuItem diagnosticsToolStripMenuItem;
+
+        private void InitializeProjectDiagnosticsMenu()
+        {
+            diagnosticsToolStripMenuItem = new ToolStripMenuItem
+            {
+                Name = "diagnosticsToolStripMenuItem",
+                Text = "Diagnostics",
+                Enabled = false
+            };
+            diagnosticsToolStripMenuItem.Click += DiagnosticsToolStripMenuItem_Click;
+            menuStrip1.Items.Add(diagnosticsToolStripMenuItem);
+        }
+
+        private void RefreshProjectDiagnosticsState()
+        {
+            if (diagnosticsToolStripMenuItem != null)
+                diagnosticsToolStripMenuItem.Enabled = CurrentProject != null;
+        }
+
+        private void DiagnosticsToolStripMenuItem_Click(object sender, EventArgs e) => ExceptionGuard.Run(() =>
+        {
+            if (CurrentProject == null)
+            {
+                MessageBox.Show("No data project is loaded.");
+                return;
+            }
+
+            using (var form = new DiagnosticsForm(CurrentProject.BuildIndex().Diagnostics))
+                form.ShowDialog(this);
+        });
+    }
+}
